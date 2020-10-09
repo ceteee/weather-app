f0f5bb230b58ebeba777c7d730a87e9210851f47
ceteee <christiantambunan55@gmail.com>
2020-10-09 17:28:53 +0700

    setup deploying to heroku

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ const forecast = require('./utils/forecast')
 // console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const port = process.env.PORT || 3000
+
+
+
 const publicdirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialPath = path.join(__dirname, '../templates/partials')
@@ -128,6 +132,6 @@ app.get('/help/*', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' +port)
 })
